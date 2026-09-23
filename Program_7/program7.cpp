04f1fdec66b7cd0f4ab31cf40cff35144092fdef
Kartiki210 <puranikkartiki@gmail.com>
2026-09-23 20:21:27 +0530

    Add Student class to track object count
    Implement Student class with static count of instances

---
+++ Program_7/program7.cpp
@@ -0,0 +1,28 @@
+#include <iostream>  // Includes input/output library
+using namespace std; // Allows use of cout without std::
+
+class Student
+{
+public:
+    static int count; // Static variable shared by all objects
+
+    // Constructor
+    Student()
+    {
+        count++; // Increases count whenever an object is created
+    }
+};
+
+// Initializes the static variable
+int Student::count = 0;
+
+int main()
+{
+    // Creating three objects of Student class
+    Student s1, s2, s3;
+
+    // Displays the total number of objects created
+    cout << Student::count;
+
+    return 0; // Indicates successful program execution
+}
